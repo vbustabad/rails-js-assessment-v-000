d48681e8e0a7e2f777c31dd91cff64c5c9350391
Veronica Bustabad <vbustabad@gmail.com>
2018-08-31 15:06:23 +0000

    Created the main routes for the application

--- adopt-a-dog-application/config/routes.rb
+++ adopt-a-dog-application/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#home'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
+  get '/owners/new' => 'owners#new'
+  post '/owners' => 'owners#create'
+  resources :dogs
+  resources :shelters
 end
