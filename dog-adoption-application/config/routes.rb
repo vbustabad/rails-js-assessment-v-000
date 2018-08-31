c707ed1ead4ea5f882b057c65f7d5192cb67d83d
Veronica Bustabad <vbustabad@gmail.com>
2018-08-31 14:35:34 +0000

    Created the database tables for the various models of the application

--- dog-adoption-application/config/routes.rb
+++
@@ -1,8 +0,0 @@
-root 'application#home'
-get '/signup' => 'users#new'
-post '/users' => 'users#create'
-get '/login' => 'sessions#new'
-post '/sessions' => 'sessions#create'
-get '/logout' => 'sessions#destroy'
-delete '/logout' => 'sessions#destroy'
-resources :dogs
