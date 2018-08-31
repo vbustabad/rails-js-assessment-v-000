38bac29045e4b338e1a0cbd18b9f2880eba3d3a9
Veronica Bustabad <vbustabad@gmail.com>
2018-08-31 14:49:13 +0000

    Created the controllers for the various models of the application

--- adopt-a-dog-application/app/controllers/application_controller.rb
+++ adopt-a-dog-application/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :logged_in?, :current_user
+
+  def home
+    render :home
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
 end
