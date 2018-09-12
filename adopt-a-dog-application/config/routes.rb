55b5a499ebaba36ddee66eb4b0e93c67c87a8898
Veronica Bustabad <vbustabad@gmail.com>
2018-09-12 16:29:37 +0000

    Added the routes for the sessions

--- adopt-a-dog-application/config/routes.rb
+++ adopt-a-dog-application/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root 'application#home'
   resources :users
   resources :shelters
   resources :owners
   resources :dogs
+  get '/signin' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
