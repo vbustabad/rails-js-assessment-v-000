86fd8744e40fa4df7ef0bcbd31a7a26da0bfd71a
Veronica Bustabad <vbustabad@gmail.com>
2018-11-23 11:46:26 -0800

    Modified destroy action and corresponding view for Sessions controller

--- adopt-a-dog-application/app/controllers/sessions_controller.rb
+++ adopt-a-dog-application/app/controllers/sessions_controller.rb
@@ -10,13 +10,13 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:password])
      session[:user_id] = @user.id
      redirect_to user_path(@user)
-   else
+    else
      render :new
-   end
- end
+    end
+  end
 
- def destroy
-   session.delete :user_id if session[:user_id]
-   redirect_to root_path
- end
+  def destroy
+    session.delete :user_id if session[:user_id]
+    redirect_to root_path
+  end
 end
