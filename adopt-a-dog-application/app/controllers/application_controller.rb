a67bd240996ad4f0cb78a1e9148c4592517264cc
Veronica Bustabad <vbustabad@gmail.com>
2018-11-23 11:25:43 -0800

    Refactored current user helper method in Application controller

--- adopt-a-dog-application/app/controllers/application_controller.rb
+++ adopt-a-dog-application/app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
     end
 
     def current_user
-      User.find_by(id: session[:user_id]) if session[:user_id]
+      User.find(session[:user_id]) if session[:user_id]
     end
 
     def require_admin
