bef9dd210107c0ac62576715806948a75ed6c177
Veronica Bustabad <vbustabad@gmail.com>
2018-11-23 11:03:35 -0800

    Completed Javascript functionality for Dogs show and index pages

--- adopt-a-dog-application/app/assets/javascripts/dogs.js
+++ adopt-a-dog-application/app/assets/javascripts/dogs.js
@@ -1,37 +1,55 @@
-$(document).ready(function() {
+$('.dogs.show').ready(function(){
   console.log("Document has been fully loaded.");
+ 
+  var descriptionLink = document.getElementById('detailed-description');
+  if (descriptionLink != null) {
+    descriptionLink.addEventListener("click", getBreedDescription);
+  }
 });
 
-$('#detailed-description').on('click', function(event) {
-  var searchTerm = $('#searchTerm').val()
-  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+$('.dogs.index').ready(function(){
+  console.log("Document has been executed."); 
+
+  var showBreedsButton = document.getElementById('show-breeds');
+  if (showBreedsButton != null) {
+    showBreedsButton.addEventListener("click", getListOfBreeds);
+  }
+});
+
+function parseJSON(response) {
+  return response.json()
+}
+
+function displayData(data) {
+  console.log(data);
+  const description = data[2][0];
+  document.getElementById('dog-description').innerHTML = description;
+  const url = data[3][0];
+  const wikipediaLink = document.getElementById('wikipedia-link');
+  wikipediaLink.innerHTML = "See full information.";
+  wikipediaLink.href = url;
+  wikipediaLink.target = "_blank";
+}
+
+function getBreedDescription(event) {
+  event.preventDefault();
+  const searchTerm = document.getElementById('dog-breed').dataset.breed;
+  const searchURL = `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=${searchTerm}`;
   
-  $.ajax({
-    url: url,
-    type: "GET",
-    data: data,
-    dataType: 'json',
-  }).done(function(data) {
-    $("#detailed-description").append(data);
-    alert("Data was successfully retrieved.");
-  }).fail(function() {
-    alert("Unable to retrieve data.");
+  fetch(searchURL)
+    .then(parseJSON)
+    .then(displayData)
+};
+
+function getListOfBreeds() {
+  $.get('/dogs.json', displayBreeds);
+};
+
+function displayBreeds(data) {
+  const list = document.getElementById('list-of-breeds');
+  data.dogs.forEach(function(dog){
+    const listItem = document.createElement('li');
+    listItem.textContent = dog.breed;
+    list.append(listItem);
   })
-});
-
-$('#list-of-characteristics').on('click', function(event) {
-  
-    $.ajax({
-      url: url,
-      data: data,
-      success: success,
-      dataType: dataType
-    }).done(function(data) {
-      $("#list-of-characteristics").append(data);
-      alert("Data was successfully retrieved.");
-    }).fail(function() {
-      alert("Unable to retrieve data.");
-    })
-  });
-
-
+};
