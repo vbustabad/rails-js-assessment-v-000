6b462a89d23906a866f63ed2146d453f55436b05
Veronica Bustabad <vbustabad@gmail.com>
2018-11-23 12:04:45 -0800

    Added provider and uid to User params

--- adopt-a-dog-application/app/controllers/users_controller.rb
+++ adopt-a-dog-application/app/controllers/users_controller.rb
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :admin)
+    params.require(:user).permit(:name, :email, :password, :admin, :provider, :uid)
   end
 
 end
