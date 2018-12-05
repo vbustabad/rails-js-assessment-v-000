1e083aa26c516f4a9858362e812a3ffcba23393f
Veronica Bustabad <vbustabad@gmail.com>
2018-12-04 18:21:14 -0800

    Added button listener and Javascript functionality for displaying dogs on Shelters show page

--- adopt-a-dog-application/app/assets/javascripts/shelters.js
+++ adopt-a-dog-application/app/assets/javascripts/shelters.js
@@ -1,19 +1,32 @@
 $('.shelters.show').ready(function(){
     console.log("Document has been loaded onto the browser.");
     
-    getListOfDogs();
+    addButtonListener()
   });  
-
-  function getListOfDogs() {
-    $.get('/dogs.json', addDogsToDOM);
-  }
   
   function addDogsToDOM(data) {
     const dogsList = document.getElementById('list-of-dogs');
+    const dogsSortedAlphabeticallyByName = data.dogs.sort((a, b) => a.name.localeCompare(b.name));
+    const dogsAvailableForAdoptionAtShelter = dogsSortedAlphabeticallyByName.filter(dog => dog.adopted === false); 
 
-    data.dogs.forEach(function(dog) {
+    dogsAvailableForAdoptionAtShelter.forEach(function(dog) {
       const listItem = document.createElement('li');
       listItem.textContent = dog.name;
-      dogsList.append(listItem);
-    })
+      dogsList.append(listItem)
+    }) 
   };
+
+  function addButtonListener() {
+    const button = document.getElementById('dogs-button');
+    console.log("Button is...", button)
+
+    button.addEventListener("click", function(event) {
+      event.preventDefault();
+
+      const dogsList = document.getElementById('list-of-dogs');
+      dogsList.innerHTML = "";
+
+      const id = this.dataset.id;
+      $.get(`/shelters/${id}.json`, addDogsToDOM)
+    })
+  }
